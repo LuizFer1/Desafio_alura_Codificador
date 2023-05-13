aef38d9afb52402333d48385ed11a48dc75b94f4
Luiz Fernando Dantas <luisferndantas@outlook.com>
2023-05-13 15:57:33 -0300

    funcionalidades do JS desenvolvidas

---
+++ main.js
@@ -0,0 +1,61 @@
+
+
+
+
+function criptar(){
+    var texto = document.getElementById('texto').value
+    var saida = document.getElementById('text_out')
+    console.log(texto)        
+    lista = []
+    for(x = 0 ;x <= texto.length; x++ ) {
+
+        if(texto[x] == "a"){
+            lista.push('ai')
+        }else if(texto[x] == "e"){
+            lista.push('enter')
+        }else if(texto[x] == "i"){
+            lista.push('imes')
+        }else if(texto[x] == "o"){
+            lista.push('ober')
+        }else if(texto[x] == "u"){
+            lista.push('ufat')
+        }else{
+            lista.push(texto[x])
+        }
+        console.log(texto[x])
+    }
+
+    console.log(lista.join(''))
+    saida.innerHTML = lista.join('')
+}
+
+function descripto(){
+    var texto = document.getElementById('texto').value
+    var saida = document.getElementById('text_out')
+    var lista = []
+    var chaves = [['a','ai'],['e','enter'],['i','imes'],['o',''],['u','ufat']]
+    
+
+    for(x = 0;x <= texto.length;x++) {
+    
+        if(texto[x] == "a"){
+            texto = texto.replace('ai','a')
+        }else if(texto[x] == "e"){
+            texto = texto.replace('enter','e')
+        }else if(texto[x] == "i"){
+            texto = texto.replace('imes','i')
+        }else if(texto[x] == "o"){
+            texto =texto.replace('ober','o')
+        }else if(texto[x] == "u"){
+            texto = texto.replace('ufat','u')
+        }
+        lista.push(texto[x])
+        }
+        
+        
+        
+    
+    
+    console.log(texto)
+    saida.innerHTML = lista.join('')
+}
